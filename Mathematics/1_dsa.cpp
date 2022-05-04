f13d82dd5ef7029ad0246dc549d9ca3a3f654e66
Himanshu <himanshuharsh1999@gmail.com>
2022-05-04 23:23:20 +0530

    first program of dsa

---
+++ Mathematics/1_dsa.cpp
@@ -0,0 +1,24 @@
+// finding number of digits in a number
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+// iterative solution
+int countDigit(long n){
+    int count = 0;
+    while (n!=0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    long n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<countDigit(n);
+    return 0;
+}
