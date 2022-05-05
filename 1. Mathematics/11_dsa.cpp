d2c2d142b8d78b5ba53238723aa8cf6399869888
Himanshu <himanshuharsh1999@gmail.com>
2022-05-06 01:12:22 +0530

    recursive power

---
+++ 1. Mathematics/11_dsa.cpp
@@ -0,0 +1,23 @@
+// Recursive Power
+#include<iostream>
+using namespace std;
+
+int power(int x, int n){
+    if (n==0)
+        return 1;
+    int temp = power(x, n/2);
+    temp = temp*temp;
+    if (n%2 == 0)
+        return temp;
+    else
+        return temp*x;
+}
+
+int main()
+{
+    int x, n;
+    cout<<"Enter the number and power: ";
+    cin>>x>>n;
+    cout<<power(x,n);
+    return 0;
+}
