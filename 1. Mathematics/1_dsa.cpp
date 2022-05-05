12990e2f3b39cd99d435c707b37d0ca86a59bc4d
Himanshu <himanshuharsh1999@gmail.com>
2022-05-05 22:15:12 +0530

    3 solutions added

--- 1. Mathematics/1_dsa.cpp
+++ 1. Mathematics/1_dsa.cpp
@@ -4,17 +4,27 @@
 using namespace std;
 
 // iterative solution
+// int countDigit(long n){
+//     int count = 0;
+//     while (n!=0)
+//     {
+//         n /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// recursive solution
+// int countDigit(long n){
+//     if (n==0)
+//         return 0;
+//     return 1 + countDigit(n/10);
+// }
+
+// Logarithmic Solution
 int countDigit(long n){
-    int count = 0;
-    while (n!=0)
-    {
-        n /= 10;
-        count++;
-    }
-    return count;
+    return floor(log10(n)) + 1;
 }
-
-
 
 int main()
 {
