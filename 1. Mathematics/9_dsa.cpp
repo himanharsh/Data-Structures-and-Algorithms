9fa174e32932e86c4c3e530b918e798edbcee7a2
Himanshu <himanshuharsh1999@gmail.com>
2022-05-06 00:47:12 +0530

    sorted divisors

---
+++ 1. Mathematics/9_dsa.cpp
@@ -0,0 +1,22 @@
+// Sorted Divisors
+#include<iostream>
+using namespace std;
+
+void printDivisors(int n){
+    int i;
+    for(i = 1; i*i<n; i++)
+        if (n%i == 0)
+            cout<<i<<" ";
+    for(; i>=1; i--)
+        if (n%i == 0)
+            cout<<(n/i)<<" ";
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    printDivisors(n);
+    return 0;
+}
