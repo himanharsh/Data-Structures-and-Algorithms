519073339e11c0e834dbac1b02c44d4e0d5e565a
Himanshu <himanshuharsh1999@gmail.com>
2022-05-07 23:11:56 +0530

    lookup table method

---
+++ 2. Bit Magic/15_dsa.cpp
@@ -0,0 +1,32 @@
+// Count Set Bits
+// Lookup Table Method for 32 bit numbers
+#include<iostream>
+using namespace std;
+
+int table[256];
+void initialize(){
+    table[0] = 0;
+    for (int i = 1; i<256; i++)
+        table[i] = (i&1) + table[i/2];
+}
+
+int countBits(int n){
+    int res = table[n & 0xff];
+    n = n>>8;
+    res = res + table[n & 0xff];
+    n = n>>8;
+    res = res + table[n & 0xff];
+    n = n>>8;
+    res = res + table[n & 0xff];
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    initialize();
+    cout<<countBits(n);
+    return 0;
+}
