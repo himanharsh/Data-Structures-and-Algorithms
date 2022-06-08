33a4fc88c529da8a4606c7602a98841c951ac273
Himanshu <himanshuharsh1999@gmail.com>
2022-06-08 16:36:31 +0530

    subsets

--- 3. Recursion/22_dsa.cpp
+++ 3. Recursion/22_dsa.cpp
@@ -2,8 +2,18 @@
 #include<iostream>
 using namespace std;
 
+void subsets(string &s, string curr, int i){
+    if (i == s.length()){
+        cout<<curr<<endl;
+        return;
+    }
+    subsets(s,curr,i+1);
+    subsets(s,curr+s[i],i+1);
+}
+
 int main()
 {
-    
+    string s = "ABC";
+    subsets(s,"",0);
     return 0;
 }
