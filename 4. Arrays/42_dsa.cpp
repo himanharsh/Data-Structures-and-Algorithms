02730c6d6031ca8c2817eb668c434ddea1fd16e4
Himanshu <himanshuharsh1999@gmail.com>
2022-06-16 21:23:39 +0530

    max length even-odd subarray

---
+++ 4. Arrays/42_dsa.cpp
@@ -0,0 +1,26 @@
+//maximum length even-odd subarray
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int maxEvenOdd(int *arr, int n){
+    int res = 1;
+    int curr = 1;
+    for (int i = 1; i<n; i++){
+        if ((arr[i]%2 != 0 && arr[i-1]%2 == 0) || (arr[i]%2 == 0 && arr[i-1]%2 != 0)){
+            curr++;
+            res = max(res,curr);
+        }
+        else
+            curr = 1;
+    }
+    return res;
+}
+
+int main()
+{
+    int arr[] = {7,10,11,32};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<maxEvenOdd(arr,n);
+    return 0;
+}
