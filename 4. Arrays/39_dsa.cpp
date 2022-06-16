1a67507613f351d3df04bf540d33092e47efcb0d
Himanshu <himanshuharsh1999@gmail.com>
2022-06-16 20:59:44 +0530

    trapping water

---
+++ 4. Arrays/39_dsa.cpp
@@ -0,0 +1,29 @@
+// trapping rainwater
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int water(int *arr, int n){
+    int res = 0;
+    int lmax[n], rmax[n];
+    lmax[0] = arr[0];
+    rmax[n-1] = arr[n-1];
+
+    for (int i = 1; i<n; i++)
+        lmax[i] = max(arr[i],lmax[i-1]);
+    
+    for (int i = n-2; i>=0; i--)
+        rmax[i] = max(arr[i],rmax[i+1]);
+    
+    for (int i = 1; i<n-1; i++)
+        res += min(lmax[i],rmax[i])-arr[i];
+    return res;
+}
+
+int main()
+{
+    int arr[] = {3,0,1,2,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<water(arr,n);
+    return 0;
+}
