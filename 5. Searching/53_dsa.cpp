62465db626d89207e83541d72199372c39eebb38
Himanshu <himanshuharsh1999@gmail.com>
2022-06-22 01:04:03 +0530

    first occurrence in a sorted array

---
+++ 5. Searching/53_dsa.cpp
@@ -0,0 +1,31 @@
+// index of first occurrence in a sorted array
+#include<iostream>
+using namespace std;
+
+int firstOcc(int *arr, int n, int x){
+    int low = 0, high = n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if (arr[mid]>x)
+            high = mid-1;
+        else if (arr[mid]<x)
+            low = mid+1;
+        else{
+            if (mid==0 || arr[mid-1] != arr[mid])
+                return mid;
+            else
+                high = mid-1; // going to left half
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1,10,10,10,20,20,40};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 20;
+
+    int result = firstOcc(arr,n,x);
+    (result != -1)?cout<<"The first occurrence index of "<<x<<" is "<<result:cout<<"Number not found";
+    return 0;
+}
