f9c3a09cc55a8ad6d95ae615c6bc3508040cdfe2
Himanshu <himanshuharsh1999@gmail.com>
2022-07-25 01:24:51 +0530

    leftmost repeating character

---
+++ 9. Strings/107_dsa.cpp
@@ -0,0 +1,25 @@
+// leftmost repeating character
+#include <bits/stdc++.h> 
+using namespace std; 
+
+const int CHAR=256;
+int leftMost(string &str) 
+{
+    bool visited[CHAR] = {0};
+    int res= -1;
+    for(int i=str.length()-1;i>=0;i--){
+        if(visited[str[i]])
+            res=i;
+        else
+            visited[str[i]]=true;
+    }
+    return res;
+}
+ 
+int main() 
+{ 
+    string str = "geeksforgeeks";
+    cout<<"Index of leftmost repeating character:"<<endl;
+    cout<<leftMost(str)<<endl;  
+    return 0; 
+} 
