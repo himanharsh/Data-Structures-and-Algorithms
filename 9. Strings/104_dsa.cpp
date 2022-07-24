24709d1bab1f15e67286865f9eff284abf1d72cb
Himanshu <himanshuharsh1999@gmail.com>
2022-07-25 01:16:34 +0530

    modified

--- 9. Strings/104_dsa.cpp
+++ 9. Strings/104_dsa.cpp
@@ -1,8 +1,24 @@
+// Check if a String is Subsequence of Other (iterative)
 #include<iostream>
+#include<cstring>
 using namespace std;
+
+bool isSubSeq(string s1, string s2, int n, int m){
+    int j = 0;
+    
+    for(int i = 0; i < n && j < m; i++){
+        if(s1[i] == s2[j])
+        j++;
+    }
+    return j == m;
+}
 
 int main()
 {
-    
+	string s1, s2;
+	cin>>s1>>s2;
+    int n = s1.length();
+    int m = s2.length();
+	cout<<boolalpha<<isSubSeq(s1, s2, n, m); // boolalpha to convert 1 to true and 0 to false 
     return 0;
 }
